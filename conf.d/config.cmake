82d0a37a346b7ad8cb1a2d5fa9be8eab245ae95a
Stephane Desneux <stephane.desneux@iot.bzh>
2017-06-08 13:25:58 +0200

    config.cmake: update content entry point and mimetype
    Change-Id: I671633e3b10c540aa38c8ab160136be971d575a4
    Signed-off-by: Stephane Desneux <stephane.desneux@iot.bzh>

--- conf.d/config.cmake
+++ conf.d/config.cmake
@@ -1,3 +1,21 @@
+###########################################################################
+# Copyright 2017 IoT.bzh
+#
+# author: Stephane Desneux <sdx@iot.bzh>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###########################################################################
+
 # Project Info
 # ------------------
 set(PROJECT_NAME helloworld-service)
@@ -5,9 +23,9 @@ set(PROJECT_VERSION "1.0")
 set(PROJECT_PRETTY_NAME "Hello World")
 set(PROJECT_DESCRIPTION "The name says it all!")
 set(PROJECT_URL "https://github.com/iotbzh/helloworld-service")
+set(PROJECT_ICON "icon.png")
 set(PROJECT_AUTHOR "Stephane Desneux")
 set(PROJECT_AUTHOR_MAIL "sdx@iot.bzh")
-set(PROJECT_ICON "icon.png")
 set(PROJECT_LICENCE "APL2.0")
 set(PROJECT_LANGUAGES,"C")
 
@@ -26,11 +44,12 @@ set(PROJECT_APP_TEMPLATES_DIR "conf.d/default")
 # ----------------------------------
 set(CMAKE_BUILD_TYPE "DEBUG")
 
-# Kernel selection if needed. Overload the detected compiler.
+# Kernel selection if needed. Impose a minimal version.
+# NOTE FOR NOW IT CHECKS KERNEL HOST VERSION
 # -----------------------------------------------
 #set (kernel_minimal_version 4.8)
 
-# Compiler selection if needed. Overload the detected compiler.
+# Compiler selection if needed. Impose a minimal version.
 # -----------------------------------------------
 set (gcc_minimal_version 4.9)
 
@@ -44,11 +63,11 @@ set (PKG_REQUIRED_LIST
 
 # Static constante definition
 # -----------------------------
-add_compile_options(-D_REENTRANT)
+add_compile_options()
 
 # LANG Specific compile flags set for all build types
 set(CMAKE_C_FLAGS "")
-set(CMAKE_CXX_FLAGS "-std=c++11")
+set(CMAKE_CXX_FLAGS "")
 
 # Print a helper message when every thing is finished
 # ----------------------------------------------------
@@ -60,6 +79,27 @@ set(WIDGET_MESSAGE "Install widget file using in the target : afm-util install $
 set(CMAKE_INSTALL_PREFIX $ENV{HOME}/opt)
 set(CMAKE_PREFIX_PATH ${CMAKE_INSTALL_PREFIX}/lib64/pkgconfig ${CMAKE_INSTALL_PREFIX}/lib/pkgconfig)
 set(LD_LIBRARY_PATH ${CMAKE_INSTALL_PREFIX}/lib64 ${CMAKE_INSTALL_PREFIX}/lib)
+
+# Mandatory widget Mimetype specification
+# --------------------------------------------------
+# Choose between :
+# - application/vnd.agl.service
+# - application/vnd.agl.native
+# - application/x-executable
+# - text/html
+#
+set(WIDGET_TYPE application/vnd.agl.service)
+
+# Mandatory Widget entry point file.
+# ----------------------------------------------------
+# This is the file that will be executed, loaded,...
+# at launch time by the application framework
+#
+# !IMPORTANT! : Service Widget Mimetype has to specified
+# the WIDGET_ENTRY_POINT "lib" which is the default directory
+# that holds the bindings.
+#
+set(WIDGET_ENTRY_POINT lib)
 
 # Optional dependencies order
 # ---------------------------
@@ -75,10 +115,18 @@ set(LD_LIBRARY_PATH ${CMAKE_INSTALL_PREFIX}/lib64 ${CMAKE_INSTALL_PREFIX}/lib)
 
 # Optional force binding installation
 # ------------------------------------
-set(BINDINGS_INSTALL_PREFIX /opt )
-# set(WIDGET_PREFIX DestinationPath)
+# set(BINDINGS_INSTALL_PREFIX PrefixPath )
 
 # Optional force binding Linking flag
 # ------------------------------------
 # set(BINDINGS_LINK_FLAG LinkOptions )
 
+# Optional force package prefix generation, like widget
+# -----------------------------------------------------
+# set(PACKAGE_PREFIX DestinationPath)
+
+# Optional Application Framework security token
+# and port use for remote debugging.
+#------------------------------------------------------------
+#set(AFB_TOKEN   ""      CACHE PATH "Default AFB_TOKEN")
+#set(AFB_REMPORT "1234" CACHE PATH "Default AFB_TOKEN")
