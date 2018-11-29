14f6e4e5a32f4b6b3d6d9597a4e52d98e1bc70b8
Johann CAHIER <johann.cahier@iot.bzh>
2018-11-29 10:02:52 +0000

    Update disclamer dates

--- helloworld-afb/helloworld-service-binding.c
+++ helloworld-afb/helloworld-service-binding.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016, 2017 "IoT.bzh"
+ * Copyright (C) 2016-2018 "IoT.bzh"
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
